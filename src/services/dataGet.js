d38976aa431ec24d540b2cd7202f8b6b7cd0b2ec
Frank Campo Castilla <fdccastilla@gmail.com>
2023-03-29 18:00:26 -0500

    Update dataGet.js
    fixed error when get service of static page

--- src/services/dataGet.js
+++ src/services/dataGet.js
@@ -1,13 +1,18 @@
 export const getInfo = async (url) => {
     const is_not_in_index_of = -1
-    if (url.indexOf('staticPageId') !== is_not_in_index_of ) {
-        const variable = url.slice(0, 47) + '@' + url.slice(47)
-        const response = await fetch(variable + '&format=json')
-        const data = await response.json()
-        return data.mainSection[0].rightSection[0].JSON
-    } else if(url.indexOf('isLanding') !== is_not_in_index_of){
-        const response = await fetch(url + '&format=json')
-        const data = await response.json()
-        return data.contents[0].mainSection[0].responseContentItems[0].JSON
+    try{
+        if (url.indexOf('staticPageId') !== is_not_in_index_of ) {
+            let preset = new Date()
+            const variable = url.slice(0, 47) + preset.getTime() + url.slice(47)
+            const response = await fetch(variable + '&format=json')
+            const data = await response.json()
+            return data.mainSection[0].rightSection[0].JSON
+        } else if(url.indexOf('isLanding') !== is_not_in_index_of){
+            const response = await fetch(url + '&format=json')
+            const data = await response.json()
+            return data.contents[0].mainSection[0].responseContentItems[0].JSON
+        }
+    } catch(error) {
+        alert('Error al cargar la Url ', error)
     }
 }
